3f5c27d2572983d3896a2efa2733c4250870c951
21812 <21812@BOOK-HVHBH9VF4E>
2023-04-30 17:33:55 +0900

    3.4.3.1 요구사항1 - index.html응답하기(실습완료)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +28,33 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	//TODO 1-1 : InputStream을 한줄로 읽기 위해 BufferedReader을 생성한다.
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in,"utf-8"));
+        	
+        	//TODO 1-2 :  readLine 메소드를 활용해 라인별로 HTTP 요청 정보를 읽는다.
+        	String line = br.readLine();
+        	
+        	//TODO 1-3 : 요청 정보 천체를 출력한다.
+        	log.debug("all request : {}", line);
+        	
+        	//TODO 1-4 : line이 null값일 경우를 대비해서 예외처리를 해준다.
+        	if(line == null) return;
+
+        	//TODO 1-5 : 헤더의 마지막을 출력한다.
+        	
+        	
+        	//TODO 2-1 : 첫 번째 요청을 추출한다.
+        	String[] tokens = line.split(" ");
+        	// tokens[0] = GET, tokens[1] = /index.html
+        	
+        	while(!line.equals("")) {
+        		line = br.readLine();
+        		log.debug("header : {}", line);
+        	}
+        	//TODO 3-1 : 요청 url에 해당하는 파일을 webapp에서 읽어 전달
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            //byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp"+tokens[1]).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
